afcf703ef4c3d1d8fd94829a7615160c8159233b
Leila Skinner <beileiwang@gmail.com>
2020-04-17 16:17:42 -0500

    set up redux in index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './components/App/App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./components/App/App";
+import registerServiceWorker from "./registerServiceWorker";
+import { createStore, combineReducers, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import logger from "redux-logger";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const feedbackReducer = (state = {}, action) => {
+  return state;
+};
+
+const storeInstance = createStore(
+  combineReducers({ feedbackReducer }),
+  applyMiddleware(logger)
+);
+
+ReactDOM.render(
+  <Provider store={storeInstance}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
 registerServiceWorker();
