43be78db5db782957a879cc2d38a3984de996429
Leila Skinner <beileiwang@gmail.com>
2020-04-18 14:52:18 -0500

    review page sibmit feedback to server and serve save datat in DB

---
+++ server/routes/feedback.router.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const router = express.Router();
+const pool = require("../modules/pool");
+
+router.post("/", (req, res) => {
+  const newFeedback = req.body;
+  const queryText = `INSERT INTO "feedback" ("feeling", "understanding", "support", "comments")
+    VALUES ($1, $2, $3, $4);`;
+  pool
+    .query(queryText, [
+      newFeedback.feeling,
+      newFeedback.understanding,
+      newFeedback.support,
+      newFeedback.comments,
+    ])
+    .then((responseFromDb) => {
+      res.sendStatus(200);
+    })
+    .catch((error) => {
+      console.log("POST ERROR: ", error);
+      res.sendStatus(500);
+    });
+});
+
+module.exports = router;
--- src/components/ReviewPage/ReviewPage.js
+++ src/components/ReviewPage/ReviewPage.js
@@ -1,9 +1,42 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import Button from "@material-ui/core/Button";
+import axios from "axios";
 
 class ReviewPage extends Component {
+  submitFeedback = (event) => {
+    const feedback = {
+      feeling: this.props.store.feelingReducer.feeling,
+      understanding: this.props.store.understandingReducer.understanding,
+      support: this.props.store.supportReducer.support,
+      comments: this.props.store.commentsReducer.comments,
+    };
+    axios
+      .post("/feedback", feedback)
+      .then((response) => {
+        console.log(response.data);
+        this.props.history.push("/success");
+      })
+      .catch((error) => {
+        console.log("POST SERVER ERROR: ", error);
+      });
+  };
+
   render() {
-    return <div></div>;
+    return (
+      <div>
+        <h2>Review Your Feedback</h2>
+        <p>Feelings: {this.props.store.feelingReducer.feeling}</p>
+        <p>
+          Understanding: {this.props.store.understandingReducer.understanding}
+        </p>
+        <p>Support: {this.props.store.supportReducer.support}</p>
+        <p>Comments: {this.props.store.commentsReducer.comments}</p>
+        <Button variant="contained" onClick={this.submitFeedback}>
+          SUBMIT
+        </Button>
+      </div>
+    );
   }
 }
 
